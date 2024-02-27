33c2ff07cb1c425a40af35e974b4b0f8476cd670
Joaquín  Custodio <joaquindiaz66@gmail.com>
2024-02-27 15:23:07 +0100

    se ha agregado marcas

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -8,12 +8,76 @@ import { ApiService } from '../services/api.service';
 })
 export class BrandsComponent implements OnInit {
   public resultBrands: Array<any> = [];
+  public editingMarca: any = null;
+  public loc: string = location.pathname
+  public nuevaMarca: any = {
+    nombre: "",
+   
+    
+  };
   constructor(public apiService: ApiService) {}
 
   ngOnInit(): void {
+   this.obtenerMarca()
+  }
+
+  obtenerMarca(){
     this.apiService.getAllBrands().subscribe((data =>{
-      console.log(data)
+     console.log(data)
       this.resultBrands = data.results
     }))
   }
+
+  borrarMarca(marca: any):void{
+    if (confirm('¿Estás seguro de que deseas borrar esta marca?')) {
+      this.apiService.deleteResource(marca.url).subscribe(
+        response => {
+         
+          console.log('Marca borrado correctamente');
+        },
+        error => {
+          console.error('Error al borrar la marca:', error);
+        }
+      );
+    }
+  }
+
+
+  anadirMarca() {
+  
+    this.apiService.createResource('marcas', this.nuevaMarca).subscribe(
+      (response) => {
+        alert('Marca añadido correctamente');
+        location.reload();
+      },
+      (error) => {
+        console.error('Error al añadir la marca:', error);
+      }
+    );
+  }
+
+
+  editarMarca(marca: any) {
+    this.editingMarca = { ...marca }; 
+  }
+  cancelarEdicion() {
+    this.editingMarca = null;
+  }
+
+  guardarCambios() {
+   this.apiService.updateResource(this.editingMarca.url , this.editingMarca).subscribe(
+    response => {
+         
+      alert('Marca Actualizado correctamente');
+      location.reload()
+
+    },
+    error => {
+      console.error('Error al actualizar la marca:', error);
+    }
+   )
+    this.editingMarca = null;
+  }
+
+
 }
